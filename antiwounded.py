0de0073b9dccc312f82d0f736d4bdef308153c56
SkinN <SkinN@users.noreply.github.com>
2016-03-04 07:29:24 +0000

    Delete antiwounded.py

--- antiwounded.py
+++
@@ -1,7 +0,0 @@
-class antiwounded:
-    def __init__(s):
-        s.Title='Anti-Wounded'
-        s.Author='SkinN'
-        s.ResourceId=1045
-        s.Version=V(1,0,1)
-    def CanBeWounded(s,p,a):return False
